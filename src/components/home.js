a45a7457c28a72c5c2d80f0884666dc933ae0a0b
Ben Li <ben.li@qantas.com.au>
2019-02-18 14:37:45 +1100

    fetch data

---
+++ src/components/home.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+function Home() {
+  const [hits, setHits] = useState([]);
+  const [query, setQuery] = useState('redux');
+  const [isError, setIsError] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+      if (!response.ok) {
+        throw new Error('Response not ok');
+      }
+      const data = await response.json();
+      setHits(data.hits);
+    }catch(e) {
+      setIsError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query]);
+
+	function handleOnChange(e) {
+		e.preventDefault();
+		setQuery(e.target.value);
+	}
+
+	return (
+		<>
+			<input type="text" onChange={handleOnChange} />
+			{ isError && <h1> something is wrong </h1> }
+			{
+	      !isError && <ul>
+          {
+            hits.map((item, index) => <li key={index}>{item.title}</li>)
+          }
+        </ul>
+			}
+		</>
+	)
+}
+
+export default Home;
