1131e8ab46c4be5a4c30cf20a2765fff0fc835c6
Ben Li <ben.li@qantas.com.au>
2019-04-29 10:33:58 +1000

    move fetch function into useEffect

--- src/components/home.js
+++ src/components/home.js
@@ -5,41 +5,40 @@ function Home() {
   const [query, setQuery] = useState('redux');
   const [isError, setIsError] = useState(false);
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
-      if (!response.ok) {
-        throw new Error('Response not ok');
-      }
-      const data = await response.json();
-      setHits(data.hits);
-    }catch(e) {
-      setIsError(true);
-    }
-  };
-
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://hn.algolia.com/api/v1/search?query=${query}`);
+        if (!response.ok) {
+          throw new Error('Response not ok');
+        }
+        const data = await response.json();
+        setHits(data.hits);
+      }catch(e) {
+        setIsError(true);
+      }
+    };
     fetchData();
   }, [query]);
 
-	function handleOnChange(e) {
-		e.preventDefault();
-		setQuery(e.target.value);
-	}
+  function handleOnChange(e) {
+    e.preventDefault();
+    setQuery(e.target.value);
+  }
 
-	return (
-		<>
-			<input type="text" value={query} onChange={handleOnChange} />
-			{ isError && <h1> something is wrong </h1> }
-			{
-	      !isError && <ul>
-          {
-            hits.map((item, index) => <li key={index}>{item.title}</li>)
-          }
+  return (
+    <>
+      <input type="text" value={query} onChange={handleOnChange} />
+      { isError && <h1> something is wrong </h1> }
+      {
+        !isError && <ul>
+        {
+          hits.map((item, index) => <li key={index}>{item.title}</li>)
+        }
         </ul>
-			}
-		</>
-	)
+      }
+    </>
+  )
 }
 
 export default Home;
